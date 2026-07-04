b68ee184346b8b8c660729ebdda716fe20942783
Tejas Waykole <tejaswaykole8@gmail.com>
2026-07-04 12:16:00 +0530

    fix(order): remove double shell nesting

--- app/order/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { CustomerMobileShell } from "@/components/layout/customer-mobile-shell";
-
-export default function OrderLayout({ children }: { children: React.ReactNode }) {
-  return <CustomerMobileShell>{children}</CustomerMobileShell>;
-}
